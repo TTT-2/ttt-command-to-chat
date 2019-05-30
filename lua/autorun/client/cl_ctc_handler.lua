eb4e89f218a0dcea1c4b8b1881fdd806b54769b8
Tim Goll <tim@timgoll.de>
2019-05-30 08:34:45 +0200

    featurecomplete

--- lua/autorun/client/cl_ctc_handler.lua
+++ lua/autorun/client/cl_ctc_handler.lua
@@ -14,16 +14,53 @@ if CLIENT then
         end
 
         -- preprocess colors
-        print("===================================================================================")
-        PrintTable(CTC.variables.colors)
-        print("===================================================================================")
+        local found_default, found_highlight = false, false
         for name, color_tbl in pairs(CTC.variables.colors) do
-            print("in loop")
-            print(name)
-            PrintTable(color_tbl)
             CTC.variables.colors[name] = Color(color_tbl[1] or 255, color_tbl[2] or 255, color_tbl[3] or 255, color_tbl[4] or 255)
-            PrintTable(CTC.variables.colors[name])
+
+            if name == 'default' then found_default = true end
+            if name == 'highlight' then found_highlight = true end
         end
+
+        -- make sure the two default colors are always set
+        if not found_default then
+            CTC.variables.colors['default'] = Color(151, 211, 255, 255)
+        end
+        if not found_highlight then
+            CTC.variables.colors['highlight'] = Color(215, 240, 240, 255)
+        end
+
+        -- register commands
+        for name, _ in pairs(CTC.commands) do
+            concommand.Add('ctc_' .. name, function(ply, cmd, args)
+                if ply ~= LocalPlayer() then return end
+
+                CTC_Command(name)
+            end)
+        end
+
+        -- register help command
+        concommand.Add('ctc_defaults_list_all', function(ply, cmd, args)
+            if ply ~= LocalPlayer() then return end
+
+            for name, command_tbl in pairs(CTC.commands) do
+                local translated = ''
+                if command_tbl.desc.localized == false then
+                    translated = command_tbl.desc.text
+                else
+                    translated = LANG.GetTranslation(command_tbl.desc.text)
+                end
+
+                local line_tbl = {
+                    CTC.variables.colors['highlight'],
+                    CTC.settings.prefix .. name,
+                    CTC.variables.colors['default'],
+                    ': ' .. translated
+                }
+                
+                chat.AddText(unpack(line_tbl))
+            end
+        end)
     end)
 
     hook.Add('OnPlayerChat', 'CTC_Player_Chat', function(player, text, teamOnly, playerIsDead)
@@ -38,10 +75,21 @@ if CLIENT then
         if CTC.commands[command] == nil then return end
 
         -- command is valid --> execute
-        CTC_PrintChat(command)
+        chat.AddText(CTC.variables.colors['default'], '>> ', CTC.variables.colors['highlight'], text)
+        CTC_RunFirst(command)
 
         return true -- prevent issued command from beeing printed to chat
     end)
+
+    function CTC_RunFirst(command)
+        if CTC.settings.run_first == 'chat' then
+            CTC_PrintChat(command)
+            CTC_Run_Command(command)
+        else
+            CTC_Run_Command(command)
+            CTC_PrintChat(command)
+        end
+    end
 
     function CTC_PrintChat(command)
         -- stop if no chat prints are needed
@@ -86,6 +134,23 @@ if CLIENT then
 
             chat.AddText(unpack(print_string_ex))
         end
+    end
+
+    function CTC_Run_Command(command)
+        -- stop if no commands are needed
+        if CTC.commands[command].console == nil then return end
+
+        for _, line in ipairs(CTC.commands[command].console) do
+            RunConsoleCommand(line)
+        end
+    end
+
+    function CTC_Command(command)
+        -- command is not found inside the data table
+        if CTC.commands[command] == nil then return end
+
+        -- command is valid --> execute
+        CTC_RunFirst(command)
     end
 
 end
--- lua/autorun/server/sv_ctc_defaults.lua
+++ lua/autorun/server/sv_ctc_defaults.lua
@@ -11,7 +11,9 @@ if SERVER then
             },
             colors = {
                 default = {151, 211, 255, 255},
-                highlight = {73, 170, 73, 255},
+                highlight = {215, 240, 240, 255},
+
+                heading = {85, 150, 100, 255},
                 white = {255, 255, 255, 255},
                 url = {10, 85, 140, 255}
             }
@@ -19,37 +21,50 @@ if SERVER then
         text = {
             Deutsch = {
                 ttt_ctc_help_line_1 = "Besuche %url%https://github.com/TimGoll/ttt-command-to-chat %default%um mehr Informationen über dieses Addon zu bekommen.",
-                ttt_ctc_help_line_2 = "%highlight%Auflistung aller Befehle:",
-                ttt_ctc_info_data_print = "Du bist $p$ und es ist $t$ Uhr, du spielst auf: {servername}!"
+                ttt_ctc_help_line_2 = "%heading%Auflistung aller Befehle:",
+                ttt_ctc_info_data_print = "Du bist $p$ und es ist $t$ Uhr, du spielst auf: {servername}!",
+                ttt_ctc_info_desc = "Gibt ein paar generelle Informationen raus."
             },
             English = {
                 ttt_ctc_help_line_1 = "Visit %url%https://github.com/TimGoll/ttt-command-to-chat %default%for more information about this addon.",
-                ttt_ctc_help_line_2 = "%highlight%List of all available commands:",
-                ttt_ctc_info_data_print = "You are %white%$p$%default% and it is %white%$t$%default% (on the %white%$d$%default%), you're playing on {servername}!"
+                ttt_ctc_help_line_2 = "%heading%List of all available commands:",
+                ttt_ctc_info_data_print = "You are %white%$p$%default% and it is %white%$t$%default% (on the %white%$d$%default%), you're playing on {servername}!",
+                ttt_ctc_info_desc = "Prints general information."
             }
         },
         commands = {
             help = {
+                desc = {
+                    localized = false,
+                    text = "Lists all commands"
+                },
                 chat = {
                     "ttt_ctc_help_line_1",
                     "ttt_ctc_help_line_2"
                 },
                 console = {
-                    "ctc_help"
+                    "ctc_defaults_list_all"
                 }
             },
             info = {
+                desc = {
+                    text = "ttt_ctc_info_desc"
+                },
                 chat = {
                     "ttt_ctc_info_data_print"
                 }
             },
             multicommand = {
+                desc = {
+                    localized = false,
+                    text = "Runs multiple commands at once"
+                },
                 localized = false,
                 chat = {
                     "Running multiple commands!"
                 },
                 console = {
-                    "xmenu",
+                    "xgui",
                     "shopeditor"
                 }
             }
